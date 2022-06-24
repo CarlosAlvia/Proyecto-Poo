ec07255fb63bf825e7ff159205875d0fab32becd
“andresACF” <andalcor@espol.edu.ec>
2022-06-24 00:02:58 -0500

    Esta si es la buena

--- src/main/java/Personas/Usuario.java
+++ src/main/java/Personas/Usuario.java
@@ -9,5 +9,40 @@ package Personas;
  * @author User
  */
 public class Usuario {
+    protected String cedula;
+    protected String nombres;
+    protected String apellidos;
+    protected String user;
+    protected int celular;
     
+    public String GetCedula(){
+        return this.cedula;
+    }
+    public String GetNombres(){
+        return this.nombres;
+    }
+    public String GetApellidos(){
+        return this.apellidos;
+    }
+    public String GetUser(){
+        return this.user;
+    }
+    public int GetCelular(){
+        return this.celular;
+    }
+    public void setCedula(String cedula){
+        this.cedula=cedula;
+    }
+    public void setNombres(String nombres){
+        this.nombres=nombres;
+    }
+    public void setApellidos(String apellidos){
+        this.apellidos=apellidos;
+    }
+    public void setUser(String user){
+        this.user=user;
+    }
+    public void setCelular(int celular){
+        this.celular=celular;
+    }
 }
