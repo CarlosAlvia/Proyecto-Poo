1e8f9704dcb924aae1b7f5b8de845ada46444b4d
cjgovea <cjgovea@espol.edu.ec>
2022-06-24 00:33:59 -0500

    Atributos getter y setters

--- src/main/java/com/dacon/proyectopoo/Entretenimiento.java
+++ src/main/java/com/dacon/proyectopoo/Entretenimiento.java
@@ -9,5 +9,29 @@ package com.dacon.proyectopoo;
  * @author User
  */
 public class Entretenimiento {
+    protected int dias;
+    protected String actividades;
+    protected String paquetes;
     
+    //Getters
+    public int Getdias(){
+        return this.dias;
+    }
+    public String Getactividades(){
+        return this.actividades;
+    }
+    public String Getpaquetes(){
+        return this.paquetes;
+    }
+    
+    //Setters
+    public void setdias(int dias){
+        this.dias=dias;
+    }
+    public void setactividades(String actividades){
+        this.actividades=actividades;
+    }
+    public void setpaquetes(String paquetes){
+        this.paquetes=paquetes;
+    }
 }
