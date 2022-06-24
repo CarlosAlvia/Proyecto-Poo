9c37bd546f23c5471a4bd783dc0cb7b6e1ae81c1
CarlosAlvia <cjalvia@espol.edu.ec>
2022-06-24 00:42:55 -0500

    cambio

--- src/main/java/com/dacon/proyectopoo/SistemaDeReservasTuristicas.java
+++ src/main/java/com/dacon/proyectopoo/SistemaDeReservasTuristicas.java
@@ -3,11 +3,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.dacon.proyectopoo;
-
+import java.util.ArrayList;
+import Personas.Usuario;
 /**
  *
  * @author CJAA
  */
 public class SistemaDeReservasTuristicas {
-    
+    private ArrayList<Usuario> listaUsuario;
 }
