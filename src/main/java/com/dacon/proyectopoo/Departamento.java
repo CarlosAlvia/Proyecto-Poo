7478df426b9762e672625429fc900a54b3fb1596
CarlosAlvia <cjalvia@espol.edu.ec>
2022-06-24 00:29:28 -0500

    cambio

--- src/main/java/com/dacon/proyectopoo/Departamento.java
+++ src/main/java/com/dacon/proyectopoo/Departamento.java
@@ -8,6 +8,27 @@ package com.dacon.proyectopoo;
  *
  * @author CJAA
  */
-public class Departamento {
+public class Departamento extends Hospedaje{
+    private int numeroHabitaciones;
+    private boolean wifi;
+    private boolean piscina;
     
+    public int getNumeroHabitaciones(){
+        return this.numeroHabitaciones;
+    }
+    public boolean getWifi(){
+        return this.wifi;
+    }
+    public boolean getPiscina(){
+        return this.piscina;
+    }
+    public void setNumeroHabitaciones(int numeroHabitaciones){
+        this.numeroHabitaciones=numeroHabitaciones;
+    }
+    public void setWifi(boolean wifi){
+        this.wifi=wifi;
+    }
+    public void setPiscina(boolean piscina){
+        this.piscina=piscina;
+    }
 }
