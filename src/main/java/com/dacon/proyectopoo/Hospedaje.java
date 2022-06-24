056dbfd204dd1fac6d97018c3a3bbd6dcde8c070
cjgovea <cjgovea@espol.edu.ec>
2022-06-24 00:45:53 -0500

    Atributos getter y setters

--- src/main/java/com/dacon/proyectopoo/Hospedaje.java
+++ src/main/java/com/dacon/proyectopoo/Hospedaje.java
@@ -8,6 +8,22 @@ package com.dacon.proyectopoo;
  *
  * @author User
  */
-public class Hospedaje {
+public class Hospedaje extends Servicio{
+    protected String fechaEntrada;
+    protected String fechaSalida;
     
+    //Getters
+    public String GetfechaEntrada(){
+        return this.fechaEntrada;
+    }
+    public String GetfechaSalida(){
+        return this.fechaSalida;
+    }
+    //Setters
+    public void setfechaEntrada(String fechaEntrada){
+        this.fechaEntrada=fechaEntrada;
+    }
+    public void setfechaSalida(String fechaSalida){
+        this.fechaSalida=fechaSalida;
+    }
 }
