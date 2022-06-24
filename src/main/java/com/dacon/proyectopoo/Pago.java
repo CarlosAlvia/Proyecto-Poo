1810626b184790eb563962fef15d2b23416db0eb
CarlosAlvia <cjalvia@espol.edu.ec>
2022-06-24 00:38:18 -0500

    cambio

--- src/main/java/com/dacon/proyectopoo/Pago.java
+++ src/main/java/com/dacon/proyectopoo/Pago.java
@@ -9,5 +9,33 @@ package com.dacon.proyectopoo;
  * @author CJAA
  */
 public class Pago {
+    private String fechaDePago;
+    private int identificador;
+    private Reserva reserva;
+    private double valorAPagar;
     
+    public String getFechaDePago(){
+        return this.fechaDePago;
+    }
+    public int getIdentificador(){
+        return this.identificador;
+    }
+    public Reserva getReserva(){
+        return this.reserva;
+    }
+    public double getValorAPagar(){
+        return this.valorAPagar;
+    }
+    public void setFechaDePago(String fechaDePago){
+        this.fechaDePago=fechaDePago;
+    }
+    public void setIdentificador(int identificador){
+        this.identificador=identificador;
+    }
+    public void setReserva(Reserva reserva){
+        this.reserva=reserva;
+    }
+    public void setValorAPagar(double valorAPagar){
+        this.valorAPagar=valorAPagar;
+    }
 }
