1be7effd6ddeeeb6b1d47f22cfbed86938e63778
CarlosAlvia <cjalvia@espol.edu.ec>
2022-06-25 00:15:58 -0500

    fds

--- src/main/java/com/dacon/proyectopoo/ProyectoPoo.java
+++ src/main/java/com/dacon/proyectopoo/ProyectoPoo.java
@@ -14,6 +14,6 @@ public class ProyectoPoo {
     public static void main(String[] args) {
         System.out.println("Hello World!");  
         SistemaDeReservasTuristicas a=new SistemaDeReservasTuristicas();
-        a.menu();
+        a.mostrarMenu();
     }
 }
