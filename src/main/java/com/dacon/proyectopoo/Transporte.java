5d7093c421c038d32499dda807af6f74b489f8e7
CarlosAlvia <cjalvia@espol.edu.ec>
2022-06-26 04:21:05 -0500

    fds

--- src/main/java/com/dacon/proyectopoo/Transporte.java
+++ src/main/java/com/dacon/proyectopoo/Transporte.java
@@ -10,5 +10,11 @@ package com.dacon.proyectopoo;
  */
 public class Transporte extends Servicio {
     private Vehiculo vehiculo;
-    
+    @Override
+    public void mostrarReserva(){
+        super.mostrarReserva();
+        System.out.println("VEHICULO: "+this.vehiculo.getMarca()+" "+this.vehiculo.getModelo());
+        System.out.println("CÓDIGO VEHICULO: "+this.vehiculo.getId());
+        System.out.println("VALOR A PAGAR: "+this.valor);
+    }
 }
