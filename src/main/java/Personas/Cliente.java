71cd6cad403cb9cd68235e1afd9244c48ebd7381
CarlosAlvia <cjalvia@espol.edu.ec>
2022-07-01 06:27:36 -0500

    "Se completó el 'login'"

--- src/main/java/Personas/Cliente.java
+++ src/main/java/Personas/Cliente.java
@@ -20,69 +20,38 @@ import Herramientas.Funcion;
  */
 public class Cliente extends Usuario{
     private int edad;
-    private int tarjetaDeCredito;
-    private char tipoCliente;
+    private String tarjetaDeCredito;
     
-//    protected String cedula;
-//    protected String nombres;
-//    protected String apellidos;
-//    protected String user;
-//    protected String contraseña;
-//    protected int celular;
-    public Cliente(String cedula, String nombres, String apellidos, String user, String contraseña, int celular,char tipoCliente){
-        super(cedula,nombres,apellidos,user,contraseña,celular);
-        this.tipoCliente=tipoCliente;
+
+    public Cliente(String cedula, String nombres, String apellidos, String user, String contraseña, String celular,char tipoCliente){
+        super(cedula,nombres,apellidos,user,contraseña,celular,tipoCliente);
+        
     }
-    public Cliente(String cedula, String nombres, String apellidos, String user, String contraseña, int celular){
+    public Cliente(String cedula, String nombres, String apellidos, String user, String contraseña, String celular){
         super(cedula,nombres,apellidos,user,contraseña,celular);
         
     }
     public int GetEdad(){
         return this.edad;
     }
-    public int GetTarjetaDeCredito(){
+    public String GetTarjetaDeCredito(){
         return this.tarjetaDeCredito;
-    }
-    public char GetTipoCliente(){
-        return this.tipoCliente;
     }
     public void setEdad(int edad){
         this.edad=edad;
     }
-    public void setTarjetaDeCredito(int tarjetaDeCredito){
+    public void setTarjetaDeCredito(String tarjetaDeCredito){
         this.tarjetaDeCredito=tarjetaDeCredito;
     }
-    public void setTipoCliente(char tipoCliente){
-        this.tipoCliente=tipoCliente;
-    }
-    public void reservarTransporte(String nombreArchivo,ArrayList<Vehiculo> vehiculos){
+    public void reservarTransporte(ArrayList<Vehiculo> vehiculos){
         Scanner sc=new Scanner(System.in);
-        String simbolo="/***************RESERVACIÓN***************/";
-        String simbolo1="/*                                       */";
-        String simbolo2="/*****************************************/";
-        System.out.println(simbolo);
-        System.out.println(simbolo1);
-        System.out.println(simbolo2);
-        System.out.println("");
+        Reserva.mostrarCabecera();
         System.out.println("Ingrese la ciudad de reserva: ");
         String ciudad=sc.nextLine();
-        System.out.println("Ingrese la fecha desde la reserva del vehículo: ");
-        String fechaInicio=sc.nextLine();
-        System.out.println("Ingrese la fecha hasta la reserva del vehículo: ");
-        String fechaFin=sc.nextLine();
-        //Calcular cuántos días se reserva 
-        String[] arrFecha1=fechaInicio.split("/");
-        String[] arrFecha2=fechaFin.split("/");
-        int año1=Integer.valueOf(arrFecha1[2]);
-        int año2=Integer.valueOf(arrFecha2[2]);
-        int dia1=Integer.valueOf(arrFecha1[0]);
-        int dia2=Integer.valueOf(arrFecha2[0]);
-        int mes1=Integer.valueOf(arrFecha1[1]);
-        int mes2=Integer.valueOf(arrFecha2[1]);
-        LocalDate fechaIni=LocalDate.of(año1,Month.of(mes1),dia1);
-        LocalDate fechaFinal=LocalDate.of(año2,Month.of(mes2),dia2);
-        long diasReserva=ChronoUnit.DAYS.between(fechaIni,fechaFinal);
-        //fin de calcular cuántos días se reserva
+        ArrayList datos=Funcion.calcularDias();
+        long diasReserva=(Long)datos.get(0);
+        String fechaInicio=(String)datos.get(1);
+        String fechaFin=(String)datos.get(2);
         System.out.println("Elija la capacidad del vehículo");
         //El documento decía preguntar tamaño y en este caso se asemeja el atributo capacidad
         int capacidadElegida=sc.nextInt();
@@ -91,36 +60,36 @@ public class Cliente extends Usuario{
         int contador=0;
         ArrayList<Vehiculo> opciones=new ArrayList<Vehiculo>();
         for (Vehiculo vehiculo:vehiculos){
-            if(vehiculo.getCapacidad()==capacidadElegida){
+            if(vehiculo.getCapacidad()==capacidadElegida&&vehiculo.getEstado().name().equals("DISPONIBLE")){
                 contador+=1;
                 System.out.println(contador+". "+vehiculo);
                 opciones.add(vehiculo);
             }
         }
-        if (opciones.size()>0){
+        if (!opciones.isEmpty()){
             System.out.println("Elija una opción");
             int op=sc.nextInt();
             while (op<1||op>opciones.size()){
                 System.out.println("Elija una opción válida");
                 op=sc.nextInt();
             }
-            System.out.println("Usted ha elegido un "+opciones.get(op).getMarca()+" "+opciones.get(op).getModelo()+" por "+diasReserva+" días");
-            double valorPagar=diasReserva*opciones.get(op).getCostoPorDia();
+            sc.nextLine();
+            System.out.println("Usted ha elegido un "+opciones.get(op-1).getMarca()+" "+opciones.get(op-1).getModelo()+" por "+diasReserva+" días");
+            double valorPagar=diasReserva*opciones.get(op-1).getCostoPorDia();
             System.out.println("El costo a pagar es de "+valorPagar+" dólares");
             System.out.println("¿Desea reservar?");
             String deseaReserva=sc.nextLine().toLowerCase();
-            
             if (deseaReserva.equals("si")||deseaReserva.equals("sí")){
+                Transporte transporte=new Transporte(ciudad,valorPagar,5.0,4,opciones.get(op-1));
                 Reserva reservaTransporte=new Reserva(fechaInicio,fechaFin,valorPagar,this,"transporte");
-                ManejoArchivos.EscribirArchivo("reservas.txt", "");
+                transporte.setReserva(reservaTransporte);
                 ManejoArchivos.EscribirArchivo("reservas.txt", reservaTransporte.toString());
                 String lineaTransporte;
-                lineaTransporte=reservaTransporte.getNumeroReserva()+","+opciones.get(op).getId()+","+valorPagar;
-                ManejoArchivos.EscribirArchivo("reservasTransporte.txt", "");
+                lineaTransporte=reservaTransporte.getNumeroReserva()+","+opciones.get(op-1).getId()+","+valorPagar;
                 ManejoArchivos.EscribirArchivo("reservasTransporte.txt", lineaTransporte);
                 //Falta iniciar el código de reserva en el constructor de Reserva y ver si reserva debe tener un
                 //objeto servicio o si servicio debe tener un objeto reserva
-                Transporte transporte=new Transporte(ciudad,valorPagar,5.0,4,opciones.get(op));
+                
                 transporte.mostrarReserva();
                 //Falta crear el constructor de servicio y el transporte
                 //public Transporte(String ciudad, double valor, Reserva reserva, double puntuacion, int identificador,Vehiculo vehiculo)
@@ -129,7 +98,6 @@ public class Cliente extends Usuario{
         }else{
             System.out.println("No hay vehículos disponibles");
         }
-        
     }
     public void reservarEntretenimiento(String nombreArchivo){
         
