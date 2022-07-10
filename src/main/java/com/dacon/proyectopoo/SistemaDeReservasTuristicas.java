088fff26ceade329ffe3a903d2b92f92492fa82c
“andresACF” <andalcor@espol.edu.ec>
2022-07-10 00:04:38 -0500

    Casteo de Usuario a Admin

--- src/main/java/com/dacon/proyectopoo/SistemaDeReservasTuristicas.java
+++ src/main/java/com/dacon/proyectopoo/SistemaDeReservasTuristicas.java
@@ -30,7 +30,7 @@ public class SistemaDeReservasTuristicas {
         ArrayList<String[]> datosUsuarios=Funcion.generarArreglo("usuarios.txt");
         ArrayList<Usuario> usuarios=new ArrayList();
         for (String[] ele:datosUsuarios){
-            if (ele[5].equals("A")){
+            if (ele[6].equals("A")){
                 Usuario usuario=new Administrador(ele[0],ele[1],ele[2],ele[3],ele[4],ele[5],ele[6].charAt(0));
                 usuarios.add(usuario);
             }else{
@@ -99,8 +99,7 @@ public class SistemaDeReservasTuristicas {
         System.out.println(simbolo2);
         System.out.println("");
         System.out.println("1. Consultar Reservas");
-        System.out.println("2. Reservar Transporte");
-        System.out.println("3. Salir");
+        System.out.println("2. Salir");
         System.out.println("\n");
     }
     public void mostrarMenuCliente(){
@@ -156,6 +155,28 @@ public class SistemaDeReservasTuristicas {
             }
         }while(!entrada.equals("6"));
     }
+    public void iniciarAdmin(){
+        String entrada = "";
+        do{
+            mostarMenuAdmin();
+            System.out.print("Ingrese opcion:");
+            entrada = sc.nextLine();
+            switch(entrada){
+                case "1":
+                    Administrador admin=(Administrador)this.usuarioConectado;
+                    admin.consultarReservas();
+                    break;
+                case "2":
+                        System.out.println("Adios");
+                    break;
+                default:
+                    //la opcion ingreada no esta dentro de las opciones del menu
+                    System.out.println("Opcion invalida");
+                    break;
+                        
+            }
+        }while(!entrada.equals("2"));
+    }
     
     public ArrayList<Usuario> getListaUsuario(){
         return this.listaUsuario;
@@ -169,10 +190,9 @@ public class SistemaDeReservasTuristicas {
         System.out.println(" ");
         sistema.verificarDatosCliente();
         System.out.println();
-        sistema.iniciar();
-        if (sistema.usuarioConectado.getTipoUsuario()=='A'){
-            sistema.mostarMenuAdmin();
-            
+        
+        if (String.valueOf(sistema.usuarioConectado.getTipoUsuario()).equals("A")){
+            sistema.iniciarAdmin();
             }
         else{
             sistema.mostrarMenuCliente();
