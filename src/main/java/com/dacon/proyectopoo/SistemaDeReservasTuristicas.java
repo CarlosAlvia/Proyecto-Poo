dcf9c91fa41dd118f13e0d53e01962aa4fed5325
CarlosAlvia <CJAA@CJAA-PC>
2022-07-10 09:39:00 -0500

    "Se comentaron los métodos de la clase"

--- src/main/java/com/dacon/proyectopoo/SistemaDeReservasTuristicas.java
+++ src/main/java/com/dacon/proyectopoo/SistemaDeReservasTuristicas.java
@@ -3,8 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.dacon.proyectopoo;
-import Enum.Estado;
-import Enum.Transmision;
 import java.util.ArrayList;
 import Personas.Usuario;
 import java.util.Scanner;
@@ -27,7 +25,13 @@ public class SistemaDeReservasTuristicas {
         System.out.println("");
         System.out.println(signo);
         System.out.println("");
+    }
+    public void ingresar(){
+        //Se lee el archivo de usuarios para conseguir los datos
         ArrayList<String[]> datosUsuarios=Funcion.generarArreglo("usuarios.txt");
+        //Se crea una lista de usarios y se crean los respectivos usuarios, sean clientes o administradores
+        //Para ello se verifica que si el tipo de usuario es "A" significa que es administrador, caso contrario es cliente
+        //Se agregan los usuarios a la lista
         ArrayList<Usuario> usuarios=new ArrayList();
         for (String[] ele:datosUsuarios){
             if (ele[6].equals("A")){
@@ -38,18 +42,23 @@ public class SistemaDeReservasTuristicas {
                 usuarios.add(usuario);
             }
         }
+        //Se asigna la lista al sistema
         this.listaUsuario=usuarios;
         Usuario usuarioCorrecto=null;
         do{
+            //Se solicita el usuario y contraseña
             System.out.println("USUARIO: ");
             String usuarioIngresado=sc.nextLine();
             System.out.println("CONTRASEÑA: ");
             String contrasenaIngresada=sc.nextLine();
+            //Se recorre la lista de usuarios para verificar si existe alguno que coincida con los datos ingresados
+            //si es el caso, se asigna ese usuario
             for (Usuario u: this.listaUsuario){
                 if (u.getUser().equals(usuarioIngresado)&&u.getContraseña().equals(contrasenaIngresada)){
                     usuarioCorrecto=u;
                 }
             }
+            //Si al terminarse la ejecución del lazo no se asignó un usuario, se solicitan otros datos
             if(usuarioCorrecto==null){
                 System.out.println("Usuario y/o contraseña inválido(s), inténtelo otra vez");
                 System.out.println(" ");
@@ -59,30 +68,45 @@ public class SistemaDeReservasTuristicas {
     }
     public void verificarDatosCliente(){
         Scanner sc=new Scanner(System.in);
+        //Se verifica que el usuario se de clase Cliente
         if(this.usuarioConectado.getClass()==Cliente.class){
             int edad=0;
             String tarjetaDeCredito;
+            //Se leen los datos del archivo clientes
             ArrayList<String[]> datosClientes=Funcion.generarArreglo("clientes.txt");
+            //Se recorre la información de clientes
             for (String[] ele:datosClientes){
+                //Se busca la información que le corresponde al usuario ingresado en el sistema
+                //por su cédula
                 if (ele[0].equals(this.usuarioConectado.getCedula())){
+                    //Se verifica que tenga dato de edad
                     if(ele[1].equals("sd")){
                         do{
+                            //En caso de no tenerlo, se solicita la edad
                             System.out.println("Ingrese su edad");
                             edad=sc.nextInt();
                             sc.nextLine();
+                            //Si la edad es inválida se muestra un mensaje y se vuelve a 
+                            //ejecutar la solicitud de una edad válida
                             if(edad<=0){
                                 System.out.println("Ingrese una edad válida");
+                            }else{
+                                ((Cliente)this.usuarioConectado).setEdad(edad);
                             }
                         }while(edad<=0);
-                        ((Cliente)this.usuarioConectado).setEdad(edad);
+                    //Si la edad sí estaba en los datos, se la define con el método setEdad
                     }else{
                         ((Cliente)this.usuarioConectado).setEdad(Integer.valueOf(ele[1]));
                     }
+                    //Se verifica que tenga el dato tarjetaDeCredito 
+                    //En el documento de clientes se usa sd que significa: sin datos
                     if(ele[2].equals("sd")){
+                            //Se solicita la tarjeta de crédito
                             System.out.println("Ingrese su tarjetaDeCredito");
                             tarjetaDeCredito=sc.nextLine();
                         ((Cliente)this.usuarioConectado).setTarjetaDeCredito(tarjetaDeCredito);
                     }else{
+                        //Si el dato estaba en el documento, se lo define con el método SetTarjetaDeCredito
                         ((Cliente)this.usuarioConectado).setTarjetaDeCredito(ele[2]);
                     }
                 }
@@ -116,16 +140,17 @@ public class SistemaDeReservasTuristicas {
         System.out.println("5. Consultar Reserva");
         System.out.println("6. Salir");
         System.out.println("\n");
-        //System.out.println(((Cliente)this.cliente).GetEdad());
-        //System.out.println(((Cliente)this.cliente).GetTarjetaDeCredito());
     }
     public void iniciar(){
         String entrada = "";
         do{
+            //Se muestra el menú al cliente
             mostrarMenuCliente();
+            //Se le solicita una opción
             System.out.print("Ingrese opcion:");
             entrada = sc.nextLine();
             switch(entrada){
+                //Se verifica si la opción es válida y qué acción debe cumplirse
                 case "1":
                     ((Cliente)this.usuarioConectado).reservarHospedaje();
                     break;
@@ -144,23 +169,28 @@ public class SistemaDeReservasTuristicas {
                     ((Cliente)this.usuarioConectado).consultarReservas();
                     break; 
                 case "6":
-                    //mostramos mensaje de finalizacion
-                    System.out.println("Adios");
+                    //se muestra el mensaje de despedida
+                    System.out.println("Adiós");
                     break;
                 default:
-                    //la opcion ingreada no esta dentro de las opciones del menu
+                    //la opcion ingresada no está dentro de las opciones del menu
                     System.out.println("Opcion invalida");
                     break;
                         
             }
+        //Se verifica si el usuario seleccionó la opción de salida, caso contrario,se
+        //muestra el menú otra vez
         }while(!entrada.equals("6"));
     }
     public void iniciarAdmin(){
         String entrada = "";
         do{
+            //Se muestra el menú
             mostarMenuAdmin();
+            //Se solicita una opción
             System.out.print("Ingrese opcion:");
             entrada = sc.nextLine();
+            //Se verifica su validez y qué debe ejecutarse
             switch(entrada){
                 case "1":
                     Administrador admin=(Administrador)this.usuarioConectado;
@@ -170,11 +200,13 @@ public class SistemaDeReservasTuristicas {
                         System.out.println("Adios");
                     break;
                 default:
-                    //la opcion ingreada no esta dentro de las opciones del menu
+                    //la opción ingreada no esta dentro de las opciones del menu
                     System.out.println("Opcion invalida");
                     break;
                         
             }
+        //Se verifica si el usuario seleccionó la opción de salida, caso contrario, se muestra el menú
+        //otra vez
         }while(!entrada.equals("2"));
     }
     
@@ -185,16 +217,19 @@ public class SistemaDeReservasTuristicas {
         this.listaUsuario=listaUsuario;
     }
     public static void main(String[] args){
+        //Se instancia el sistema y se ejecutan los métodos respectivos
         SistemaDeReservasTuristicas sistema=new SistemaDeReservasTuristicas();
         sistema.mostrarBienvenida();
+        sistema.ingresar();
         System.out.println(" ");
-        sistema.verificarDatosCliente();
-        System.out.println();
-        
+        //Se verifica si el uuario es administrador o cliente para saber qué 
+        //inicio se debe usar
         if (String.valueOf(sistema.usuarioConectado.getTipoUsuario()).equals("A")){
             sistema.iniciarAdmin();
             }
         else{
+            //Antes de mostrarle el menú al cliente, se verifica si tiene datos por llenar
+            sistema.verificarDatosCliente();
             sistema.iniciar();
         }
     }
