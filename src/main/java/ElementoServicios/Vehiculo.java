60862bb105bccbb32068e44fcb193dc88bd05811
“andresACF” <andalcor@espol.edu.ec>
2022-07-10 21:41:41 -0500

    Correciones

--- src/main/java/ElementoServicios/Vehiculo.java
+++ src/main/java/ElementoServicios/Vehiculo.java
@@ -25,6 +25,18 @@ public class Vehiculo {
     private double costoPorDia;
     private Transmision transmision;
     
+    /**
+     *
+     * @param id
+     * @param marca
+     * @param modelo
+     * @param placa
+     * @param color
+     * @param capacidad
+     * @param estado
+     * @param costoPorDia
+     * @param transmision
+     */
     public Vehiculo(String id,String marca,String modelo,String placa, String color, int capacidad, Estado estado,double costoPorDia, Transmision transmision ){
         this.id=id;
         this.marca=marca;
@@ -37,60 +49,154 @@ public class Vehiculo {
         this.transmision=transmision;
     }
     
+    /**
+     *
+     * @return
+     */
     public String getId(){
         return this.id;
     }
+
+    /**
+     *
+     * @return
+     */
     public String getMarca(){
         return this.marca;
     }
+
+    /**
+     *
+     * @return
+     */
     public String getModelo(){
         return this.modelo;
     }
+
+    /**
+     *
+     * @return
+     */
     public String getPlaca(){
         return this.placa;
     }
+
+    /**
+     *
+     * @return
+     */
     public String getColor(){
         return this.color;
     }
+
+    /**
+     *
+     * @return
+     */
     public int getCapacidad(){
         return this.capacidad;
     }
+
+    /**
+     *
+     * @return
+     */
     public Estado getEstado(){
         return this.estado;
     }
+
+    /**
+     *
+     * @return
+     */
     public double getCostoPorDia(){
         return this.costoPorDia;
     }
+
+    /**
+     *
+     * @return
+     */
     public Transmision getTransmision(){
         return this.transmision;
     }
+
+    /**
+     *
+     * @param id
+     */
     public void setId(String id){
         this.id=id;
     }
+
+    /**
+     *
+     * @param marca
+     */
     public void setMarca(String marca){
         this.marca=marca;
     }
+
+    /**
+     *
+     * @param modelo
+     */
     public void setModelo(String modelo){
         this.modelo=modelo;
     }
+
+    /**
+     *
+     * @param placa
+     */
     public void setPlaca(String placa){
         this.placa=placa;
     }
+
+    /**
+     *
+     * @param color
+     */
     public void setColor(String color){
         this.color=color;
     }
+
+    /**
+     *
+     * @param capacidad
+     */
     public void setCapacidad(int capacidad){
         this.capacidad=capacidad;
     }
+
+    /**
+     *
+     * @param estado
+     */
     public void setEstado(Estado estado){
         this.estado=estado;
     }
+
+    /**
+     *
+     * @param costoPorDia
+     */
     public void setCostoPorDia(double costoPorDia){
         this.costoPorDia=costoPorDia;
     }
+
+    /**
+     *
+     * @param transmision
+     */
     public void setTransmision(Transmision transmision){
         this.transmision=transmision;
     }
+
+    /**
+     *
+     * @return
+     */
     @Override
     public String toString(){
         return this.marca+" "+this.modelo+" "+this.color+" con transmisión: "+this.transmision+" costo por día: "+this.costoPorDia;
