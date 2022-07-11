e8949d1123383594a17f8caaafad580c8361b1d5
“andresACF” <andalcor@espol.edu.ec>
2022-07-10 21:41:33 -0500

    Correciones

--- src/main/java/ElementoServicios/Paquete.java
+++ src/main/java/ElementoServicios/Paquete.java
@@ -24,70 +24,142 @@ public class Paquete {
    String fechaDisponible;
    int numeroPersonas;
 
+    /**
+     *
+     * @return
+     */
     public String getCiudadPa() {
         return ciudadPa;
     }
 
+    /**
+     *
+     * @param ciudadPa
+     */
     public void setCiudadPa(String ciudadPa) {
         this.ciudadPa = ciudadPa;
     }
 
+    /**
+     *
+     * @return
+     */
     public ArrayList<Paquete> getPaquetes() {
         return paquetes;
     }
 
+    /**
+     *
+     * @param paquetes
+     */
     public void setPaquetes(ArrayList<Paquete> paquetes) {
         this.paquetes = paquetes;
     }
  
+    /**
+     *
+     * @return
+     */
     public String getNombrePaquete() {
         return nombrePaquete;
     }
 
+    /**
+     *
+     * @param nombrePaquete
+     */
     public void setNombrePaquete(String nombrePaquete) {
         this.nombrePaquete = nombrePaquete;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getInformacion() {
         return informacion;
     }
 
+    /**
+     *
+     * @param informacion
+     */
     public void setInformacion(String informacion) {
         this.informacion = informacion;
     }
 
+    /**
+     *
+     * @return
+     */
     public double getCostoP() {
         return costoP;
     }
 
+    /**
+     *
+     * @param costoP
+     */
     public void setCostoP(double costoP) {
         this.costoP = costoP;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getDiasSalida() {
         return diasSalida;
     }
 
+    /**
+     *
+     * @param diasSalida
+     */
     public void setDiasSalida(String diasSalida) {
         this.diasSalida = diasSalida;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getFechaDisponible() {
         return fechaDisponible;
     }
 
+    /**
+     *
+     * @param fechaDisponible
+     */
     public void setFechaDisponible(String fechaDisponible) {
         this.fechaDisponible = fechaDisponible;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getNumeroPersonas() {
         return numeroPersonas;
     }
 
+    /**
+     *
+     * @param numeroPersonas
+     */
     public void setNumeroPersonas(int numeroPersonas) {
         this.numeroPersonas = numeroPersonas;
     }
     
+    /**
+     *
+     * @param informacion
+     * @param costoP
+     * @param diasSalida
+     * @param fechaDisponible
+     * @param numeroPersonas
+     */
     public Paquete(String informacion, double costoP, String diasSalida, String fechaDisponible, int numeroPersonas) {
         this.informacion = informacion;
         this.costoP = costoP;
@@ -95,6 +167,16 @@ public class Paquete {
         this.fechaDisponible = fechaDisponible;
         this.numeroPersonas = numeroPersonas;
     }
+
+    /**
+     *
+     * @param ciudadPa
+     * @param nombrePaquete
+     * @param informacion
+     * @param costoP
+     * @param diasSalida
+     * @param fechaDisponible
+     */
     public Paquete(String ciudadPa,String nombrePaquete, String informacion, double costoP, String diasSalida, String fechaDisponible) {
         this.ciudadPa=ciudadPa;
         this.nombrePaquete = nombrePaquete;
@@ -104,6 +186,9 @@ public class Paquete {
         this.fechaDisponible = fechaDisponible; 
     }
     
+    /**
+     *
+     */
     public Paquete() {    
     }
 }
