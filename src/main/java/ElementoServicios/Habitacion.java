c0db00766148667f46ad85efbd1a055b6c378192
“andresACF” <andalcor@espol.edu.ec>
2022-07-10 21:41:21 -0500

    Correciones

--- src/main/java/ElementoServicios/Habitacion.java
+++ src/main/java/ElementoServicios/Habitacion.java
@@ -22,11 +22,22 @@ public class Habitacion{
     private int numeroDeHabitacion;
     private Estado estado;
 
-    
+    /**
+     *
+     */
     public Habitacion(){
          
      }
     
+    /**
+     *
+     * @param codigoHotel
+     * @param tipoHabitacion
+     * @param precio
+     * @param capacidad
+     * @param numerodeHabitacion
+     * @param estado
+     */
     public Habitacion(String codigoHotel, TipoHabitacion tipoHabitacion, double precio, int capacidad, int numerodeHabitacion, Estado estado) {
         this.codigoHotel = codigoHotel;
         this.tipoHabitacion = tipoHabitacion;
@@ -36,43 +47,101 @@ public class Habitacion{
         this.estado = estado;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getCodigoHotel() {
         return codigoHotel;
     }
 
+    /**
+     *
+     * @param codigoHotel
+     */
     public void setCodigoHotel(String codigoHotel) {
         this.codigoHotel = codigoHotel;
     }
 
     //Getters
+
+    /**
+     *
+     * @return
+     */
     public TipoHabitacion getTipoHabitacion(){
         return this.tipoHabitacion;
     }
+
+    /**
+     *
+     * @return
+     */
     public double getPrecio(){
         return this.precio;
     }
+
+    /**
+     *
+     * @return
+     */
     public int getCapacidad(){
         return this.capacidad;
     }
+
+    /**
+     *
+     * @return
+     */
     public int getNumeroDeHabitacion(){
         return this.numeroDeHabitacion;
     }
+
+    /**
+     *
+     * @return
+     */
     public Estado getEstado(){
         return this.estado;
     }
     //Setters
+
+    /**
+     *
+     * @param tipoHabitacion
+     */
     public void setTipoHabitacion(TipoHabitacion tipoHabitacion){
         this.tipoHabitacion=tipoHabitacion;
     }
+
+    /**
+     *
+     * @param precio
+     */
     public void setPrecio(double precio){
         this.precio=precio;
     }
+
+    /**
+     *
+     * @param capacidad
+     */
     public void setCapacidad(int capacidad){
         this.capacidad=capacidad;
     }
+
+    /**
+     *
+     * @param numerodeHabitacion
+     */
     public void setNumeroDeHabitacion(int numerodeHabitacion){
         this.numeroDeHabitacion=numerodeHabitacion;
     }
+
+    /**
+     *
+     * @param estado
+     */
     public void setEstado(Estado estado){
         this.estado=estado;
     }
